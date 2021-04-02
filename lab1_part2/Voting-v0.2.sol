2e43b27c78ea097ca76abf90d4b3019c87b35837
mellowLove <1341826597@qq.com>
2021-04-02 16:41:01 +0800

    Add files via upload

---
+++ lab1_part2/Voting-v0.2.sol
@@ -0,0 +1,64 @@
+pragma solidity >=0.4.0 <0.6.0;
+// We have to specify what version of compiler this code will compile with
+
+contract Voting {
+  /* mapping field below is equivalent to an associative array or hash.
+  The key of the mapping is candidate name stored as type bytes32 and value is
+  an unsigned integer to store the vote count
+  */
+  
+  mapping (bytes32 => uint256) public votesReceived;
+  
+  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
+  We will use an array of bytes32 instead to store the list of candidates
+  */
+  
+  bytes32[] public candidateList;
+
+  /* This is the constructor which will be called once when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  constructor(bytes32[] memory candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(bytes32 candidate) view public returns (uint256) {
+    require(validCandidate(candidate));
+    return votesReceived[candidate];
+  }
+
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForCandidate(bytes32 candidate) public {
+    require(validCandidate(candidate));
+    votesReceived[candidate] += 1;
+  }
+
+  function validCandidate(bytes32 candidate) view public returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+/*
+deployedContract.deploy({
+data: bytecode,
+arguments: [listOfCandidates.map(name =>
+web3.utils.asciiToHex(name))]
+}).send({
+from: '0x9e0C36A6995875940bbb3A79a7ccdF54BB3C6c9c',
+gas: 1500000,
+gasPrice: web3.utils.toWei('0.00003', 'ether')
+}).then((newContractInstance) => {
+deployedContract.options.address = newContractInstance.options.address
+console.log(newContractInstance.options.address)
+});
+
+deployedContract.methods.voteForCandidate(web3.utils.asciiToHex('Rama')).send({from: '0x9e0C36A6995875940bbb3A79a7ccdF54BB3C6c9c'}).then((f) => console.log(f))
+*/
